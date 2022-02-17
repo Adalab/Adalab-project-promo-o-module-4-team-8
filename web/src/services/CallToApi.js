81f5d70ed7d546a31af7e61d2b51994190a83cf9
Ico Lizhen <icolizhen@gmail.com>
2022-02-17 12:02:02 +0000

    update static server

--- web/src/services/CallToApi.js
+++ web/src/services/CallToApi.js
@@ -1,7 +1,11 @@
 const CallToApi = (data) => {
-  const remote_server = 'https://awesome-profile-cards.herokuapp.com/card';
+  const remote_server = 'https://awesomecards.herokuapp.com/#/card';
   const local_host = 'http://localhost:4000/card';
-  return fetch(local_host, {
+
+  const serverPort =
+    process.env.NODE_ENV === 'production' ? remote_server : local_host;
+
+  return fetch(serverPort, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: { 'Content-Type': 'application/json' },
